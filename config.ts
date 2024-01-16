cc37c5b3c213c8e8a9e71c3a2757b7dc55af3d60
benjaminbellwether <156847057+benjaminbellwether@users.noreply.github.com>
2024-01-16 15:19:02 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,7 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
-  maxTokens: 2000000,
+  url: "https://www.petersom.com",
+  maxPagesToCrawl: 1000,
+  outputFileName: "ps-website-output.json",
 };
